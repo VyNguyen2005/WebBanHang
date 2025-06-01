3fc7fba36b2a3595cb9240871574d1b89cb1df89
VyNguyen2005 <nguyenvyadoramy@gmail.com>
2025-06-01 23:47:25 +0700

    commit-phan-6

--- WebBanHang/Startup.cs
+++ WebBanHang/Startup.cs
@@ -65,11 +65,11 @@ namespace WebBanHang
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                   name: "areas",
+                   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapControllerRoute(
-                   name: "area",
-                   pattern: "{area=exists}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
