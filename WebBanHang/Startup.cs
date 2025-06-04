01ec597b663ab7b39ac0787269c61a2cf373d729
VyNguyen2005 <nguyenvyadoramy@gmail.com>
2025-06-04 11:01:06 +0700

    lab06_1

--- WebBanHang/Startup.cs
+++ WebBanHang/Startup.cs
@@ -27,6 +27,7 @@ namespace WebBanHang
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSession();
             services.AddControllersWithViews();
             services.AddDbContext<MyWebContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("ConnectionString")));
@@ -61,6 +62,8 @@ namespace WebBanHang
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
